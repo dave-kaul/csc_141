d90e8fc826d3ee3e1a6b940377c7360869fea7b6
Dave Kaul <davekaul@gmail.com>
2024-09-27 09:51:16 -0400

    Updated with you striking the hero

--- using_lists_to_battle_superheroes.py
+++ using_lists_to_battle_superheroes.py
@@ -4,25 +4,30 @@ import random
 import os
 # Every Superhero must have a power ranking of how much damage they can do
 os.system('cls')
-superheroes = {'Batman': 7, 'Spiderman': 9, 'Hulk': 9, 'Aquaman': 1}
+superheroes = {'Batman': 7, 'Spiderman': 9, 'Hulk': 9, 'Aquaman': 1, 'Ironman': 10}
 
 number_of_superheroes = len(superheroes) #page 44
+#print (number_of_superheroes)
 # Dictionaries have key/value pairs. P.92
 # get a list of the key values, which are the names of the heroes. p.101 (keys)
 
 superhero_names = list(superheroes.keys()) # page 101
+
+print(superhero_names)
 superhero_power_levels = list(superheroes.values()) # page 104
-your_power_level = 8
+print (superhero_power_levels)
+your_power_level = 12
+your_max_damage = 7
 print ('The heroes in the arena are:')
-for i in superhero_names:
-    print (i)
+for hero in superhero_names:
+    print (hero)
 print ('\n')
 
 # Get a random hero to fight from the list using len command
 random_number = random.randint(0, number_of_superheroes - 1) 
 superhero = superhero_names[random_number]
 hero_power = superhero_power_levels[random_number]
-print (f'You have find yourself in the arena with {superhero}, who has a power level of {hero_power}!')
+print (f'You find yourself in the arena with {superhero}, who has a power level of {hero_power}!')
 
 #print (superhero_names)
 choice =''
@@ -34,14 +39,33 @@ while True:
     choice = input("Do you (e)scape, or (f)ight?: ")
     
     if (choice == 'f'): # page 72
-        damage_to_you = random.randint(0, hero_power)
-        print (f'{superhero} strikes you, delivering {damage_to_you} points of damage.')
+
+        # We strike hero first
+        damage_to_hero = random.randint(0, your_max_damage)
+        if damage_to_hero == 0:
+            print (f'You barely miss striking {superhero}!')
+        else:
+            print (f'You stike {superhero}, delivering {damage_to_hero} points of damage.')
+        hero_power = hero_power - damage_to_hero
+
+        if hero_power <= 0:
+            print (f'You killed {superhero}!')
+            break
+        
+
+        # Hero fights you
+        damage_to_you = random.randint(0, your_max_damage)
+        
+        if damage_to_you == 0:
+            print (f'{superhero} barely misses you!')
+        else:
+            print (f'{superhero} strikes you, delivering {damage_to_you} points of damage.')
         your_power_level = your_power_level - damage_to_you
         if your_power_level <= 0:
             print (f'You have died.')
             break
         
-
+print ("Hope you had fun playing.")
 
     
 
