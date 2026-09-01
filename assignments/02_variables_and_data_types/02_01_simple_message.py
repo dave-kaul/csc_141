6ca7b489a6d41f2220da42833a7f6418d43d385c
dave-kaul <davekaul@gmail.com>
2026-09-01 13:21:21 -0400

    2nd commit

---
+++ assignments/02_variables_and_data_types/02_01_simple_message.py
@@ -0,0 +1,16 @@
+'''
+Type info here
+
+
+msg = "I feel extreme Python Power!"
+print (msg)
+msg = "Python sucks!"
+print (msg)
+
+
+print ("PYTHON IS AWESOME!")
+'''
+singer = "justin bieber"
+print (singer)
+print(singer.title())
+
